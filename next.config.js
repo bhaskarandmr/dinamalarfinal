771211eed64d8373f854cca6e2a2f68b6912efd0
bhaskarandmr <99174239+bhaskarandmr@users.noreply.github.com>
2022-08-30 13:25:02 +0530

    changesdone

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    //enter the domain or subdomain where you have WordPress installed
+    domains: ["https://w.dinamalar.com"]
+  }
+};
